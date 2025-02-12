5f7aa1b717198c70050dea7ece0fb485f0dadf94
Kassma <doerfer.cedric@gmail.com>
2025-02-12 23:05:49 +0100

    added UnitTest for Day02 Task01

---
+++ AdventOfCodeTests/AoC2015/Door02/TestTask01.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCodeTests.AoC2015.Door02;
+
+using AdventOfCode.AoC2015.Door02;
+
+[TestFixture]
+public static class TestTask01
+{
+    public static IEnumerable<TestCaseData> TestCases
+    {
+        get
+        {
+            yield return new TestCaseData(new int[,] { { 2, 3, 4 } })
+                .Returns(58)
+                .SetName("Test01");
+
+            yield return new TestCaseData(new int[,] { { 1, 1, 10 } })
+                .Returns(43)
+                .SetName("Test02");
+        }
+    }
+
+    [TestCaseSource(nameof(TestCases))]
+    public static int RunTask_ValidCases_ShouldReturnCorrectInt(int[,] equations)
+    {
+        return Task01.RunTask(equations);
+    }
+}
