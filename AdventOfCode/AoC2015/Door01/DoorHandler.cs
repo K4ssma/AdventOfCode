b97f413c531fa414cf34e905a913e21189b001cf
Kassma <doerfer.cedric@gmail.com>
2025-02-12 19:44:35 +0100

    implemented Door01 Task02

--- AdventOfCode/AoC2015/Door01/DoorHandler.cs
+++ AdventOfCode/AoC2015/Door01/DoorHandler.cs
@@ -5,7 +5,7 @@ internal class DoorHandler : IDoorHandler
     public int? OpenDoor(string inputString)
     {
         int? taskNum = UserInputUtility.ReadIntChoice(
-            [1], "Please enter which task you want to run.");
+            [1, 2], "Please enter which task you want to run.");
 
         if (taskNum == null)
         {
@@ -19,6 +19,12 @@ internal class DoorHandler : IDoorHandler
             case 1:
             {
                 taskFunc = Task01.RunTask;
+                break;
+            }
+
+            case 2:
+            {
+                taskFunc = Task02.RunTask;
                 break;
             }
 
---
+++ AdventOfCode/AoC2015/Door01/Task02.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCode.AoC2015.Door01;
+
+internal static class Task02
+{
+    public static int RunTask(string inputString)
+    {
+        int floorNum = 0;
+
+        for (int i = 0; i < inputString.Length; i++)
+        {
+            switch (inputString[i])
+            {
+                case '(':
+                {
+                    floorNum++;
+                    break;
+                }
+
+                case ')':
+                {
+                    floorNum--;
+                    break;
+                }
+
+                default:
+                {
+                    throw new FormatException($"invalid char '{inputString[i]}' occured at position {i}");
+                }
+            }
+
+            if (floorNum < 0)
+            {
+                return i + 1;
+            }
+        }
+
+        throw new ArgumentException($"there is no solution for the input {inputString}");
+    }
+}
