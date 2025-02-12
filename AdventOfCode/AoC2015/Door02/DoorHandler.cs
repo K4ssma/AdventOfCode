d0f593af75d578e0eb18187fb25c4089e92acfb9
Kassma <doerfer.cedric@gmail.com>
2025-02-12 22:25:17 +0100

    added DoorHandler for Day2 with an InputConverter

---
+++ AdventOfCode/AoC2015/Door02/DoorHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode.AoC2015.Door02;
+
+internal class DoorHandler : IDoorHandler
+{
+    public int? OpenDoor(string inputString)
+    {
+        int? taskNum = UserInputUtility.ReadIntChoice(
+            [], "Please enter the number of the task you want to run.");
+
+        if (taskNum == null)
+        {
+            return null;
+        }
+
+        Func<int[,], int> taskFunc;
+
+        switch (taskNum.Value)
+        {
+            default:
+            {
+                throw new NotImplementedException($"task number '{taskNum}' is not yet supported");
+            }
+        }
+
+        int[,] equations = InputConverter.Convert(inputString);
+
+        return taskFunc(equations);
+    }
+}
---
+++ AdventOfCode/AoC2015/Door02/InputConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode.AoC2015.Door02;
+
+using System.Text.RegularExpressions;
+
+internal static class InputConverter
+{
+    public static int[,] Convert(string inputString)
+    {
+        string[] equationStrings = inputString.Split("\r\n");
+
+        int[,] equations = new int[equationStrings.Length, 3];
+
+        Regex regex = new(@"(\d+)x(\d+)x(\d+)", RegexOptions.Compiled);
+
+        for (int i = 0; i < equationStrings.Length; i++)
+        {
+            MatchCollection matches = regex.Matches(equationStrings[i]);
+
+            equations[i, 0] = int.Parse(matches[0].Groups[0].Value);
+            equations[i, 1] = int.Parse(matches[0].Groups[1].Value);
+            equations[i, 2] = int.Parse(matches[0].Groups[2].Value);
+        }
+
+        return equations;
+    }
+}
