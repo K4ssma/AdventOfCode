7228b3ead8f868178e977b7048509052e0750f2b
Kassma <doerfer.cedric@gmail.com>
2025-02-12 17:35:03 +0100

    created "InputFetcher" class

---
+++ AdventOfCode/InputFetcher.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode;
+
+internal static class InputFetcher
+{
+    public static async Task<string> FetchInput(int year, int door)
+    {
+        string sessionCookie = Config.Instance["SessionCookie"]
+            ?? throw new ArgumentException("'SessionCookie' entry in the config is empty");
+
+        string url = Config.Instance["AdventOfCodeUrl"]
+            ?? throw new ArgumentException("'AdventOfCodeUrl' entry in the config is empty");
+
+        using HttpClient client = new();
+        client.DefaultRequestHeaders.Add("Cookie", $"session={sessionCookie}");
+
+        return await client.GetStringAsync(url + $"/{year}/day/{door}/input");
+    }
+}
