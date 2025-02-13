a5154215c89b7a5710de3cffef72eb2e58be232c
Kassma <doerfer.cedric@gmail.com>
2025-02-13 17:17:17 +0100

    now able to run Door03 Task01

--- AdventOfCode/AoC2015/Year2015Handler.cs
+++ AdventOfCode/AoC2015/Year2015Handler.cs
@@ -11,7 +11,7 @@ public class Year2015Handler : IYearHandler
     public async Task<int?> RunYear()
     {
         int? doorNum = UserInputUtility.ReadIntChoice(
-            [1, 2], "Please enter the number of the day of the task you want to run");
+            [1, 2, 3], "Please enter the number of the day of the task you want to run");
 
         if (doorNum == null)
         {
