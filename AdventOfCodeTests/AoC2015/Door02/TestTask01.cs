bb9da8a62af20f7c0a22b79a779221ea39d42c9c
Kassma <doerfer.cedric@gmail.com>
2025-02-13 15:53:43 +0100

    added another TestCase for Day02 Task01 covering input strings with multiple lines

--- AdventOfCodeTests/AoC2015/Door02/TestTask01.cs
+++ AdventOfCodeTests/AoC2015/Door02/TestTask01.cs
@@ -16,6 +16,14 @@ public static class TestTask01
             yield return new TestCaseData(new int[,] { { 1, 1, 10 } })
                 .Returns(43)
                 .SetName("Test02");
+
+            yield return new TestCaseData(new int[,]
+            {
+                { 2, 3, 4 },
+                { 1, 1, 10 },
+            })
+                .Returns(101)
+                .SetName("Test03");
         }
     }
 
