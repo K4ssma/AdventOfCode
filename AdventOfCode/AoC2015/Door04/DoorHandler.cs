36a720366d361df555544da5e70e1225f3e6248e
Kassma <doerfer.cedric@gmail.com>
2025-02-13 17:54:38 +0100

    implemented DoorHandler for Day04

---
+++ AdventOfCode/AoC2015/Door04/DoorHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode.AoC2015.Door04;
+
+internal class DoorHandler : IDoorHandler
+{
+    public int? OpenDoor(string inputString)
+    {
+        int? taskNum = UserInputUtility.ReadIntChoice(
+            [], "Please enter the number of the task you would like to run.");
+
+        if (taskNum == null)
+        {
+            return null;
+        }
+
+        Func<string, int> taskFunc;
+
+        switch (taskNum)
+        {
+            default:
+            {
+                throw new NotImplementedException($"task number '{taskNum}' is not yet supported");
+            }
+        }
+
+        return taskFunc(inputString);
+    }
+}
--- AdventOfCode/AoC2015/Year2015Handler.cs
+++ AdventOfCode/AoC2015/Year2015Handler.cs
@@ -5,13 +5,14 @@ using System.Threading.Tasks;
 using DoorHandler01 = AdventOfCode.AoC2015.Door01.DoorHandler;
 using DoorHandler02 = AdventOfCode.AoC2015.Door02.DoorHandler;
 using DoorHandler03 = AdventOfCode.AoC2015.Door03.DoorHandler;
+using DoorHandler04 = AdventOfCode.AoC2015.Door04.DoorHandler;
 
 public class Year2015Handler : IYearHandler
 {
     public async Task<int?> RunYear()
     {
         int? doorNum = UserInputUtility.ReadIntChoice(
-            [1, 2, 3], "Please enter the number of the day of the task you want to run");
+            [1, 2, 3, 4], "Please enter the number of the day of the task you want to run");
 
         if (doorNum == null)
         {
@@ -37,6 +38,12 @@ public class Year2015Handler : IYearHandler
             case 3:
             {
                 doorHandler = new DoorHandler03();
+                break;
+            }
+
+            case 4:
+            {
+                doorHandler = new DoorHandler04();
                 break;
             }
 
