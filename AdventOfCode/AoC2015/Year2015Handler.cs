47b83aae16cbc2dc4167a8dc78362daa427635c7
Kassma <doerfer.cedric@gmail.com>
2025-02-13 15:54:21 +0100

    now able to run Day02 Task01

--- AdventOfCode/AoC2015/Year2015Handler.cs
+++ AdventOfCode/AoC2015/Year2015Handler.cs
@@ -2,13 +2,17 @@
 
 using System.Threading.Tasks;
 using AdventOfCode.AoC2015.Door01;
+using AdventOfCode.AoC2015.Door02;
+
+using DoorHandler01 = AdventOfCode.AoC2015.Door01.DoorHandler;
+using DoorHandler02 = AdventOfCode.AoC2015.Door02.DoorHandler;
 
 public class Year2015Handler : IYearHandler
 {
     public async Task<int?> RunYear()
     {
         int? doorNum = UserInputUtility.ReadIntChoice(
-            [1], "Please enter the number of the day of the task you want to run");
+            [1, 2], "Please enter the number of the day of the task you want to run");
 
         if (doorNum == null)
         {
@@ -21,7 +25,13 @@ public class Year2015Handler : IYearHandler
         {
             case 1:
             {
-                doorHandler = new DoorHandler();
+                doorHandler = new DoorHandler01();
+                break;
+            }
+
+            case 2:
+            {
+                doorHandler = new DoorHandler02();
                 break;
             }
 
