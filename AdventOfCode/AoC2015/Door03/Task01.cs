c9e0fdc376fc31227176d0f4a2c1199db694713b
Kassma <doerfer.cedric@gmail.com>
2025-02-13 17:10:15 +0100

    implemented Day03 Task01

---
+++ AdventOfCode/AoC2015/Door03/Task01.cs
@@ -0,0 +1,51 @@
+﻿namespace AdventOfCode.AoC2015.Door03;
+
+internal static class Task01
+{
+    public static int RunTask(string instructionString)
+    {
+        int x = 0;
+        int y = 0;
+
+        HashSet<(int X, int Y)> visitedHouses = [(0, 0)];
+
+        foreach (char c in instructionString)
+        {
+            switch (c)
+            {
+                case '^':
+                {
+                    y++;
+                    break;
+                }
+
+                case '>':
+                {
+                    x++;
+                    break;
+                }
+
+                case 'v':
+                {
+                    y--;
+                    break;
+                }
+
+                case '<':
+                {
+                    x--;
+                    break;
+                }
+
+                default:
+                {
+                    throw new FormatException($"invalid character '{c}' occured");
+                }
+            }
+
+            visitedHouses.Add((x, y));
+        }
+
+        return visitedHouses.Count;
+    }
+}
