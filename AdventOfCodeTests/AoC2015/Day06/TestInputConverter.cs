803bc4529291be6fc447d297cd73371b04a19be8
Kassma <doerfer.cedric@gmail.com>
2025-02-15 16:15:39 +0100

    added UnitTest for InputConverter for Day06

---
+++ AdventOfCodeTests/AoC2015/Day06/TestInputConverter.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCodeTests.AoC2015.Day06;
+
+using AdventOfCode.AoC2015.Day06;
+
+[TestFixture]
+public static class TestInputConverter
+{
+    public static IEnumerable<TestCaseData> TestCases
+    {
+        get
+        {
+            yield return new TestCaseData("turn on 0,0 through 999,999")
+                .Returns((object)(new (InstructionType, Vector2Int, Vector2Int)[]
+                {
+                    (InstructionType.On, new(0, 0), new(999, 999)),
+                }))
+                .SetName("Test01");
+
+            yield return new TestCaseData("toggle 0,0 through 999,0")
+                .Returns((object)(new (InstructionType, Vector2Int, Vector2Int)[]
+                {
+                    (InstructionType.Toogle, new(0, 0), new(999, 0)),
+                }))
+                .SetName("Test02");
+
+            yield return new TestCaseData("turn off 499,499 through 500,500")
+                .Returns((object)(new (InstructionType, Vector2Int, Vector2Int)[]
+                {
+                    (InstructionType.Off, new(499, 499), new(500, 500)),
+                }))
+                .SetName("Test03");
+        }
+    }
+
+    [TestCaseSource(nameof(TestCases))]
+    public static (InstructionType Type, Vector2Int PosOne, Vector2Int PosTwo)[]
+        Convert_ValidCases_ShouldReturnCorrectInstructions(string inputString)
+    {
+        return InputConverter.Convert(inputString);
+    }
+}
