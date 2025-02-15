37e6824dc1e917cf91145ae2d470c9b640015c04
Kassma <doerfer.cedric@gmail.com>
2025-02-15 17:32:20 +0100

    Added UnitTest for Day06 Task02

---
+++ AdventOfCodeTests/AoC2015/Day06/TestTask02.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCodeTests.AoC2015.Day06;
+
+using AdventOfCode.AoC2015.Day06;
+
+[TestFixture]
+public static class TestTask02
+{
+    public static IEnumerable<TestCaseData> TestCases
+    {
+        get
+        {
+            yield return new TestCaseData((object)(new (InstructionType Type, Vector2Int PosOne, Vector2Int PosTwo)[]
+            {
+                (InstructionType.On, new(0, 0), new(0, 0)),
+            }))
+                .Returns(1)
+                .SetName("Test01");
+
+            yield return new TestCaseData((object)(new (InstructionType Type, Vector2Int PosOne, Vector2Int PosTwo)[]
+            {
+                (InstructionType.Toggle, new(0, 0), new(999, 999)),
+            }))
+                .Returns(2_000_000)
+                .SetName("Test02");
+
+            yield return new TestCaseData((object)(new (InstructionType Type, Vector2Int PosOne, Vector2Int PosTwo)[]
+            {
+                (InstructionType.Off, new(0, 0), new(999, 0)),
+            }))
+                .Returns(0)
+                .SetName("Test03");
+        }
+    }
+
+    [TestCaseSource(nameof(TestCases))]
+    public static int RunTask_ValidCases_ShouldReturnCorrectInt(
+        (InstructionType Type, Vector2Int PosOne, Vector2Int PosTwo)[] instructions)
+    {
+        return Task02.RunTask(instructions);
+    }
+}
