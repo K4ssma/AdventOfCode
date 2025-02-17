520a0f8da2dc04f4ce0b2771f70a86a859448112
Kassma <doerfer.cedric@gmail.com>
2025-02-17 21:05:17 +0100

    implemented Day07 Task02

---
+++ AdventOfCode/AoC2015/Day07/Task02.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode.AoC2015.Day07;
+
+using AdventOfCode.AoC2015.Day07.Logics.Gates;
+using AdventOfCode.AoC2015.Day07.Logics.Values;
+
+internal static class Task02
+{
+    public static int RunTask(string inputString)
+    {
+        Dictionary<string, IGate> gates = InputConverter.Convert(inputString);
+
+        ushort value = (ushort)Task01.RunTask(gates, "a");
+
+        Dictionary<string, IGate> rewiredGates = InputConverter.Convert(inputString);
+        rewiredGates["b"] = new ConstantGate(new ConstantValue(value), "b");
+
+        return Task01.RunTask(rewiredGates, "a");
+    }
+}
