3680dad399214479a9c30c6af1c44227e75eb042
Kassma <doerfer.cedric@gmail.com>
2025-02-18 18:16:34 +0100

    implemented Day08 Task02

---
+++ AdventOfCode/AoC2015/Day08/Task02.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode.AoC2015.Day08;
+
+internal static class Task02
+{
+    public static int RunTask(string inputString)
+    {
+        return inputString
+            .Split("\n")
+            .Where((string str) => str != string.Empty)
+            .Select((string str) =>
+            {
+                int codeSize = str.Length + 2;
+
+                foreach (char c in str)
+                {
+                    if (c == '\\'
+                        || c == '"')
+                    {
+                        codeSize++;
+                    }
+                }
+
+                return codeSize - str.Length;
+            })
+            .Aggregate((int curr, int next) => curr + next);
+    }
+}
