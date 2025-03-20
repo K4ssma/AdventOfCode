96c352705783f2e92d6d66490ad5cf8c2a52e3f1
Kassma <doerfer.cedric@gmail.com>
2025-03-20 20:47:06 +0100

    Added UnitTest for Day09 Task01

---
+++ AdventOfCodeTests/AoC2015/Day09/TestTask01.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCodeTests.AoC2015.Day09;
+
+using AdventOfCode.AoC2015.Day09;
+
+[TestFixture]
+public static class TestTask01
+{
+    public static IEnumerable<TestCaseData> TestCases
+    {
+        get
+        {
+            yield return new TestCaseData(new int[,]
+            {
+                { 0, 464, 518 },
+                { 464, 0, 141 },
+                { 518, 141, 0 },
+            })
+                .Returns(605)
+                .SetName("Test01");
+        }
+    }
+
+    [TestCaseSource(nameof(TestCases))]
+    public static int RunTask_ValidCases_ShouldReturnCorrectValue(int[,] distanceMatrix)
+    {
+        return Task01.RunTask(distanceMatrix);
+    }
+}
