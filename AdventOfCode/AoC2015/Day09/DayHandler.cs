38d2a30a726fe2e72b70750a039270575f3bc599
Kassma <doerfer.cedric@gmail.com>
2025-03-20 21:48:47 +0100

    now able to run Day09 Task02

--- AdventOfCode/AoC2015/Day09/DayHandler.cs
+++ AdventOfCode/AoC2015/Day09/DayHandler.cs
@@ -5,5 +5,6 @@ internal class DayHandler : IDayHandler
     public Dictionary<int, Func<string, int>> AvailableTasks { get; } = new()
     {
         { 1, (string inputString) => Task01.RunTask(InputConverter.Convert(inputString)) },
+        { 2, (string inputString) => Task02.RunTask(InputConverter.Convert(inputString)) },
     };
 }
