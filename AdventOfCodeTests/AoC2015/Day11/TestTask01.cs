e837fb4ef11024ac7f2acef63776f3c55cf8e099
Kassma <doerfer.cedric@gmail.com>
2025-03-21 19:16:17 +0100

    added UnitTest for Day11 Task01

---
+++ AdventOfCodeTests/AoC2015/Day11/TestTask01.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCodeTests.AoC2015.Day11;
+
+using AdventOfCode.AoC2015.Day11;
+
+[TestFixture]
+public static class TestTask01
+{
+    public static IEnumerable<TestCaseData> TestCases
+    {
+        get
+        {
+            yield return new TestCaseData("abcdefgh").Returns("abcdffaa").SetName("Test01");
+
+            yield return new TestCaseData("ghijklmn").Returns("ghjaabcc").SetName("Test02");
+        }
+    }
+
+    [TestCaseSource(nameof(TestCases))]
+    public static string RunTask_ValidCases_ShouldReturnValidPassword(string previousPassword)
+    {
+        return Task01.RunTask(previousPassword);
+    }
+}
