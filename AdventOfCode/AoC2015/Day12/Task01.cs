69ea6d552b004bad2e27af72198d814dc7f45fad
Kassma <doerfer.cedric@gmail.com>
2025-03-21 19:40:42 +0100

    implemented Day12 Task01

---
+++ AdventOfCode/AoC2015/Day12/Task01.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode.AoC2015.Day12;
+
+using System.Text.RegularExpressions;
+
+internal static class Task01
+{
+    public static int RunTask(string inputString)
+    {
+        Regex regex = new(@"-?\d+");
+        MatchCollection matches = regex.Matches(inputString);
+
+        return matches.Count == 0
+            ? 0
+            : matches
+                .Select((Match match) => int.Parse(match.Value))
+                .Aggregate((int curr, int next) => curr + next);
+    }
+}
