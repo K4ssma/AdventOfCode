505de937f97fdc3b95fa2bf987a582419c6addae
Kassma <doerfer.cedric@gmail.com>
2025-03-27 14:12:47 +0100

    Day13: Implemented InputConverter

---
+++ AdventOfCode/AoC2015/Day13/InputConverter.cs
@@ -0,0 +1,55 @@
+﻿namespace AdventOfCode.AoC2015.Day13;
+
+using System.Text.RegularExpressions;
+
+internal static class InputConverter
+{
+    public static int[,] Convert(string inputString)
+    {
+        Dictionary<string, Dictionary<string, int>> happinessConnections = [];
+        Dictionary<string, int> indexLookup = [];
+
+        Regex regex = new(
+            @"^(?<source>\w+)\swould\s(?<operator>gain|lose)\s(?<amount>\d+)\shappiness\sunits\sby\ssitting\snext\sto\s(?<target>\w+)\.$",
+            RegexOptions.Compiled);
+
+        foreach (string line in inputString.Trim().Split("\r\n"))
+        {
+            Match match = regex.Match(line);
+
+            if (!match.Success)
+            {
+                throw new FormatException($"the line '{line}' is not in the right format");
+            }
+
+            if (!happinessConnections.TryGetValue(match.Groups["source"].Value, out Dictionary<string, int>? connection))
+            {
+                connection = [];
+                happinessConnections.Add(match.Groups["source"].Value, connection);
+
+                indexLookup.Add(match.Groups["source"].Value, indexLookup.Count);
+            }
+
+            int amount = int.Parse(match.Groups["amount"].Value);
+
+            if (match.Groups["operator"].Value == "lose")
+            {
+                amount *= -1;
+            }
+
+            connection.Add(match.Groups["target"].Value, amount);
+        }
+
+        int[,] happinessMatrix = new int[happinessConnections.Count, happinessConnections.Count];
+
+        foreach (KeyValuePair<string, Dictionary<string, int>> connections in happinessConnections)
+        {
+            foreach (KeyValuePair<string, int> connection in connections.Value)
+            {
+                happinessMatrix[indexLookup[connections.Key], indexLookup[connection.Key]] = connection.Value;
+            }
+        }
+
+        return happinessMatrix;
+    }
+}
---
+++ AdventOfCodeTests/AoC2015/Day13/TestInputConverter.cs
@@ -0,0 +1,59 @@
+﻿namespace AdventOfCodeTests.AoC2015.Day13;
+
+using AdventOfCode.AoC2015.Day13;
+
+[TestFixture]
+public static class TestInputConverter
+{
+    [Test]
+    public static void Convert_ValidCase_ShouldConvertToCorrectMatrix()
+    {
+        // arrange
+        string inputString =
+            "Alice would gain 54 happiness units by sitting next to Bob.\r\n" +
+            "Alice would lose 79 happiness units by sitting next to Carol.\r\n" +
+            "Alice would lose 2 happiness units by sitting next to David.\r\n" +
+            "Bob would gain 83 happiness units by sitting next to Alice.\r\n" +
+            "Bob would lose 7 happiness units by sitting next to Carol.\r\n" +
+            "Bob would lose 63 happiness units by sitting next to David.\r\n" +
+            "Carol would lose 62 happiness units by sitting next to Alice.\r\n" +
+            "Carol would gain 60 happiness units by sitting next to Bob.\r\n" +
+            "Carol would gain 55 happiness units by sitting next to David.\r\n" +
+            "David would gain 46 happiness units by sitting next to Alice.\r\n" +
+            "David would lose 7 happiness units by sitting next to Bob.\r\n" +
+            "David would gain 41 happiness units by sitting next to Carol.";
+
+        // act
+        int[,] result = InputConverter.Convert(inputString);
+
+        // assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.GetLength(0), Is.EqualTo(4));
+            Assert.That(result.GetLength(1), Is.EqualTo(4));
+        });
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result[0, 0], Is.EqualTo(0));
+            Assert.That(result[0, 1], Is.EqualTo(54));
+            Assert.That(result[0, 2], Is.EqualTo(-79));
+            Assert.That(result[0, 3], Is.EqualTo(-2));
+
+            Assert.That(result[1, 0], Is.EqualTo(83));
+            Assert.That(result[1, 1], Is.EqualTo(0));
+            Assert.That(result[1, 2], Is.EqualTo(-7));
+            Assert.That(result[1, 3], Is.EqualTo(-63));
+
+            Assert.That(result[2, 0], Is.EqualTo(-62));
+            Assert.That(result[2, 1], Is.EqualTo(60));
+            Assert.That(result[2, 2], Is.EqualTo(0));
+            Assert.That(result[2, 3], Is.EqualTo(55));
+
+            Assert.That(result[3, 0], Is.EqualTo(46));
+            Assert.That(result[3, 1], Is.EqualTo(-7));
+            Assert.That(result[3, 2], Is.EqualTo(41));
+            Assert.That(result[3, 3], Is.EqualTo(0));
+        });
+    }
+}
