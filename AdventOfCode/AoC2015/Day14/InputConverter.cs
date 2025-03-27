ff5161c78e6836756fb978153916eb6e415dd42c
Kassma <doerfer.cedric@gmail.com>
2025-03-27 18:41:00 +0100

    Day14: implemented InputConverter

---
+++ AdventOfCode/AoC2015/Day14/InputConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCode.AoC2015.Day14;
+
+using System.Text.RegularExpressions;
+
+internal static class InputConverter
+{
+    public static IEnumerable<(uint Speed, uint FlightTime, uint RestingTime)> Convert(string inputString)
+    {
+        Regex regex = new(
+            @"^\w+\scan\sfly\s(?<speed>\d+)\skm/s\sfor\s(?<flightTime>\d+)\sseconds,\sbut\sthen\smust\srest\sfor\s(?<restTime>\d+)\sseconds\.$",
+            RegexOptions.Compiled);
+
+        foreach (string line in inputString.Trim().Split('\n'))
+        {
+            Match match = regex.Match(line.Trim());
+
+            yield return match.Success
+                ? ((uint Speed, uint FlightTime, uint RestingTime))(
+                uint.Parse(match.Groups["speed"].Value),
+                uint.Parse(match.Groups["flightTime"].Value),
+                uint.Parse(match.Groups["restTime"].Value))
+                : throw new FormatException($"the line '${line}' is not in the right format for conversion");
+        }
+    }
+}
---
+++ AdventOfCodeTests/AoC2015/Day14/TestInputConverter.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCodeTests.AoC2015.Day14;
+
+using AdventOfCode.AoC2015.Day14;
+
+[TestFixture]
+public static class TestInputConverter
+{
+    public static IEnumerable<TestCaseData> TestCases
+    {
+        get
+        {
+            yield return new TestCaseData(
+                "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.\r\n" +
+                "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.\r\n")
+                .Returns(new (uint FlightSpeed, uint FlightTime, uint RestTime)[]
+                {
+                    (14, 10, 127),
+                    (16, 11, 162),
+                })
+                .SetName("Test01");
+        }
+    }
+
+    [TestCaseSource(nameof(TestCases))]
+    public static (uint, uint, uint)[] Convert_ValidCases_ShouldReturnCorrectValue(string inputString)
+    {
+        return InputConverter.Convert(inputString).ToArray();
+    }
+}
