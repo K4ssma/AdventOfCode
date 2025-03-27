eab5e15a0d9466b8420fe391fb7fa62b4a1592c2
Kassma <doerfer.cedric@gmail.com>
2025-03-27 18:20:14 +0100

    Day13: implemented Task02

--- AdventOfCode/AoC2015/Day13/DayHandler.cs
+++ AdventOfCode/AoC2015/Day13/DayHandler.cs
@@ -8,5 +8,6 @@ internal class DayHandler : IDayHandler
     public Dictionary<int, Func<string, int>> AvailableTasks => new()
     {
         { 1, (string inputString) => Task01.RunTask(InputConverter.Convert(inputString)) },
+        { 2, (string inputString) => Task02.RunTask(InputConverter.Convert(inputString)) },
     };
 }
---
+++ AdventOfCode/AoC2015/Day13/Task02.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode.AoC2015.Day13;
+
+internal static class Task02
+{
+    public static int RunTask(int[,] happinessMatrix)
+    {
+        int[,] extendetMatrix = new int[happinessMatrix.GetLength(0) + 1, happinessMatrix.GetLength(1) + 1];
+
+        for (int i = 0; i < happinessMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < happinessMatrix.GetLength(1); j++)
+            {
+                extendetMatrix[i, j] = happinessMatrix[i, j];
+            }
+        }
+
+        return Task01.RunTask(extendetMatrix);
+    }
+}
