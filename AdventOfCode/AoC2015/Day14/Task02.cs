e51de4e3399a6356d921649a471a617621fcb91f
Kassma <doerfer.cedric@gmail.com>
2025-03-27 22:02:07 +0100

    Day14: implemented Task02

---
+++ AdventOfCode/AoC2015/Day14/Task02.cs
@@ -0,0 +1,71 @@
+﻿namespace AdventOfCode.AoC2015.Day14;
+
+internal static class Task02
+{
+    public static int RunTask(
+        (uint Speed, uint FlightTime, uint RestTime)[] reindeersTimes,
+        uint measuringTime)
+    {
+        if (reindeersTimes.Length == 0)
+        {
+            throw new ArgumentException("the provided array must have at least one element");
+        }
+
+        uint[] points = new uint[reindeersTimes.Length];
+        uint[] distances = new uint[reindeersTimes.Length];
+        uint[] timers = new uint[reindeersTimes.Length];
+        bool[] isResting = new bool[reindeersTimes.Length];
+
+        for (int i = 0; i < reindeersTimes.Length; i++)
+        {
+            timers[i] = reindeersTimes[i].FlightTime;
+        }
+
+        for (int i = 0; i < measuringTime; i++)
+        {
+            List<int> maxDistanceIndeces = [];
+            uint maxDistance = 0;
+
+            for (int j = 0; j < reindeersTimes.Length; j++)
+            {
+                if (!isResting[j])
+                {
+                    distances[j] += reindeersTimes[j].Speed;
+                }
+
+                timers[j]--;
+
+                if (timers[j] == 0)
+                {
+                    timers[j] = isResting[j]
+                        ? reindeersTimes[j].FlightTime
+                        : reindeersTimes[j].RestTime;
+
+                    isResting[j] = !isResting[j];
+                }
+
+                if (distances[j] < maxDistance)
+                {
+                    continue;
+                }
+
+                if (distances[j] == maxDistance)
+                {
+                    maxDistanceIndeces.Add(j);
+                    continue;
+                }
+
+                maxDistanceIndeces = [];
+                maxDistanceIndeces.Add(j);
+                maxDistance = distances[j];
+            }
+
+            foreach (int index in maxDistanceIndeces)
+            {
+                points[index]++;
+            }
+        }
+
+        return (int)points.Max();
+    }
+}
---
+++ AdventOfCodeTests/AoC2015/Day14/TestTask02.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCodeTests.AoC2015.Day14;
+
+using AdventOfCode.AoC2015.Day14;
+
+[TestFixture]
+public static class TestTask02
+{
+    public static IEnumerable<TestCaseData> TestCases
+    {
+        get
+        {
+            yield return new TestCaseData(
+                new (uint, uint, uint)[]
+                {
+                    (14, 10, 127),
+                    (16, 11, 162),
+                },
+                1000u)
+                .Returns(689)
+                .SetName("Test01");
+        }
+    }
+
+    [TestCaseSource(nameof(TestCases))]
+    public static int RunTask_ValidCases((uint Speed, uint FlightTime, uint RestTime)[] reindeers, uint measureTime)
+    {
+        return Task02.RunTask(reindeers, measureTime);
+    }
+}
