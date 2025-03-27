406fc5740d3548cbba75063d73b485150a7ad82c
Kassma <doerfer.cedric@gmail.com>
2025-03-27 19:14:04 +0100

    Day14: implemented Task01

---
+++ AdventOfCode/AoC2015/Day14/Task01.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode.AoC2015.Day14;
+
+internal static class Task01
+{
+    public static int RunTask(
+        IEnumerable<(uint FlightSpeed, uint FlightDuration, uint RestDuration)> reindeerTimes,
+        uint measuringTime)
+    {
+        uint maxDistance = 0;
+
+        foreach ((uint FlightSpeed, uint FlightDuration, uint RestDuration) reindeer in reindeerTimes)
+        {
+            uint cycleDuration = reindeer.FlightDuration + reindeer.RestDuration;
+            uint completedCycles = measuringTime / cycleDuration;
+
+            uint remainingTime = measuringTime - (completedCycles * cycleDuration);
+
+            uint distance = (completedCycles * reindeer.FlightDuration * reindeer.FlightSpeed)
+              + (Math.Min(remainingTime, reindeer.FlightDuration) * reindeer.FlightSpeed);
+
+            maxDistance = Math.Max(maxDistance, distance);
+        }
+
+        return (int)maxDistance;
+    }
+}
---
+++ AdventOfCodeTests/AoC2015/Day14/TestTask01.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCodeTests.AoC2015.Day14;
+
+using AdventOfCode.AoC2015.Day14;
+
+[TestFixture]
+public static class TestTask01
+{
+    public static IEnumerable<TestCaseData> TestCases
+    {
+        get
+        {
+            yield return new TestCaseData(
+                new (uint FlightSpeed, uint FlightTime, uint RestTime)[]
+                {
+                    (14, 10, 127),
+                    (16, 11, 162),
+                },
+                1000u)
+                .Returns(1120)
+                .SetName("Test01");
+        }
+    }
+
+    [TestCaseSource(nameof(TestCases))]
+    public static int RunTask_ValidCases_ShouldReturnMaxDistance(
+        IEnumerable<(uint FlightSpeed, uint FlightTime, uint RestTime)> reindeerTimes,
+        uint measuringTime)
+    {
+        return Task01.RunTask(reindeerTimes, measuringTime);
+    }
+}
