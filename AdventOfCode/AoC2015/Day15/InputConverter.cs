34b0d768a9d1a0d58b1e5b08a62a480b1cae9244
Kassma <doerfer.cedric@gmail.com>
2025-03-28 16:19:34 +0100

    Day15: Implemented InputConverter

---
+++ AdventOfCode/AoC2015/Day15/InputConverter.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode.AoC2015.Day15;
+
+using System.Text.RegularExpressions;
+
+internal static class InputConverter
+{
+    public static IEnumerable<(int Capacity, int Durability, int Flavor, int Texture, int Calories)>
+        Convert(string inputString)
+    {
+        Regex regex = new(
+            @"^\w+:\scapacity\s(?<capacity>(-)?\d+),\sdurability\s(?<durability>(-)?\d+),\sflavor\s(?<flavor>(-)?\d+),\stexture\s(?<texture>(-)?\d+),\scalories\s(?<calories>(-)?\d+)$",
+            RegexOptions.Compiled);
+
+        return inputString
+            .Trim()
+            .Split('\n')
+            .Select((string line) =>
+            {
+                Match match = regex.Match(line.Trim());
+
+                if (!match.Success)
+                {
+                    throw new FormatException($"the line '{line}' does not have the right format for conversion");
+                }
+
+                return (int.Parse(match.Groups["capacity"].Value),
+                    int.Parse(match.Groups["durability"].Value),
+                    int.Parse(match.Groups["flavor"].Value),
+                    int.Parse(match.Groups["texture"].Value),
+                    int.Parse(match.Groups["calories"].Value));
+            });
+    }
+}
---
+++ AdventOfCodeTests/AoC2015/Day15/TestInputConverter.cs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCodeTests.AoC2015.Day15;
+
+using AdventOfCode.AoC2015.Day15;
+
+[TestFixture]
+public static class TestInputConverter
+{
+    public static IEnumerable<TestCaseData> TestCases
+    {
+        get
+        {
+            yield return new TestCaseData(
+                "Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8\r\n" +
+                "Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3")
+                .Returns(new (int, int, int, int, int)[]
+                {
+                    (-1, -2, 6, 3, 8),
+                    (2, 3, -2, -1, 3),
+                })
+                .SetName("Test01");
+        }
+    }
+
+    [TestCaseSource(nameof(TestCases))]
+    public static (int Capacity, int Durability, int Flavor, int Texture, int Calories)[]
+        Convert_ValidCases_ShouldReturnCorrectConversion(string inputString)
+    {
+        return InputConverter.Convert(inputString).ToArray();
+    }
+}
