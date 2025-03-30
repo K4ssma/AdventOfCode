e27fa69c78e81e7dc18ab4d6b34cbe2da1206bee
Kassma <doerfer.cedric@gmail.com>
2025-03-30 21:54:08 +0200

    Day16: Implemented InputConverter.

---
+++ AdventOfCode/AoC2015/Day16/InputConverter.cs
@@ -0,0 +1,46 @@
+﻿namespace AdventOfCode.AoC2015.Day16;
+
+using System.Text.RegularExpressions;
+
+internal static class InputConverter
+{
+    public static Dictionary<string, byte>[] Convert(string inputString)
+    {
+        Dictionary<string, Regex> regexes = new Dictionary<string, Regex>
+        {
+            { "children", new(@"children:\s(?<count>\d+)", RegexOptions.Compiled) },
+            { "cats", new(@"cats:\s(?<count>\d+)", RegexOptions.Compiled) },
+            { "samoyeds", new(@"samoyeds:\s(?<count>\d+)", RegexOptions.Compiled) },
+            { "pomeranians", new(@"pomeranians:\s(?<count>\d+)", RegexOptions.Compiled) },
+            { "akitas", new(@"akitas:\s(?<count>\d+)", RegexOptions.Compiled) },
+            { "vizslas", new(@"vizslas:\s(?<count>\d+)", RegexOptions.Compiled) },
+            { "goldfish", new(@"goldfish:\s(?<count>\d+)", RegexOptions.Compiled) },
+            { "trees", new(@"trees:\s(?<count>\d+)", RegexOptions.Compiled) },
+            { "cars", new(@"cars:\s(?<count>\d+)", RegexOptions.Compiled) },
+            { "perfumes", new(@"perfumes:\s(?<count>\d+)", RegexOptions.Compiled) },
+        };
+
+        return inputString
+            .Trim()
+            .Split('\n')
+            .Select((string line) =>
+            {
+                Dictionary<string, byte> properties = new();
+
+                foreach ((string propertyName, Regex regex) in regexes)
+                {
+                    Match match = regex.Match(line);
+
+                    if (!match.Success)
+                    {
+                        continue;
+                    }
+
+                    properties.Add(propertyName, byte.Parse(match.Groups["count"].Value));
+                }
+
+                return properties;
+            })
+            .ToArray();
+    }
+}
---
+++ AdventOfCodeTests/AoC2015/Day16/TestInputConverter.cs
@@ -0,0 +1,57 @@
+﻿namespace AdventOfCodeTests.AoC2015.Day16;
+
+using AdventOfCode.AoC2015.Day16;
+
+[TestFixture]
+public static class TestInputConverter
+{
+    public static IEnumerable<TestCaseData> TestCases
+    {
+        get
+        {
+            yield return new TestCaseData(
+                "Sue: " +
+                "children: 3, " +
+                "cats: 7, " +
+                "samoyeds: 2, " +
+                "pomeranians: 3, " +
+                "akitas: 0, " +
+                "vizslas: 0, " +
+                "goldfish: 5, " +
+                "trees: 3, " +
+                "cars: 2, " +
+                "perfumes: 1")
+                .Returns(new (string, byte)[][]
+                {
+                    [
+                        ("children", 3),
+                        ("cats", 7),
+                        ("samoyeds", 2),
+                        ("pomeranians", 3),
+                        ("akitas", 0),
+                        ("vizslas", 0),
+                        ("goldfish", 5),
+                        ("trees", 3),
+                        ("cars", 2),
+                        ("perfumes", 1),
+                    ],
+                })
+                .SetName("Test01");
+        }
+    }
+
+    [TestCaseSource(nameof(TestCases))]
+    public static (string, byte)[][] Convert_ValidCases_ShouldReturnCorrectConversion(string inputString)
+    {
+        Dictionary<string, byte>[] auntsProperties = InputConverter.Convert(inputString);
+
+        return auntsProperties
+            .Select((Dictionary<string, byte> auntProperties) =>
+            {
+                return auntProperties
+                    .Select((KeyValuePair<string, byte> property) => (property.Key, property.Value))
+                    .ToArray();
+            })
+            .ToArray();
+    }
+}
