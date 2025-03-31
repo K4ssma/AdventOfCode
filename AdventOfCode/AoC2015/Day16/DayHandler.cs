857680efcca76fd9d2e198dcd8dd83db80eb0be5
Kassma <doerfer.cedric@gmail.com>
2025-03-31 17:52:53 +0200

    Day16: Implemented Task01

--- AdventOfCode/AoC2015/Day16/DayHandler.cs
+++ AdventOfCode/AoC2015/Day16/DayHandler.cs
@@ -5,7 +5,22 @@ using System.Collections.Generic;
 
 internal class DayHandler : IDayHandler
 {
+    private static Dictionary<string, byte> TargetAunt => new()
+    {
+        { "children", 3 },
+        { "cats", 7 },
+        { "samoyeds", 2 },
+        { "pomeranians", 3 },
+        { "akitas", 0 },
+        { "vizslas", 0 },
+        { "goldfish", 5 },
+        { "trees", 3 },
+        { "cars", 2 },
+        { "perfumes", 1 },
+    };
+
     public Dictionary<int, Func<string, int>> AvailableTasks => new()
     {
+        { 1, (string inputString) => (int)Task01.RunTask(TargetAunt, InputConverter.Convert(inputString))! },
     };
 }
---
+++ AdventOfCode/AoC2015/Day16/Task01.cs
@@ -0,0 +1,24 @@
+﻿namespace AdventOfCode.AoC2015.Day16;
+
+internal static class Task01
+{
+    public static uint? RunTask(Dictionary<string, byte> targetAuntProperties, Dictionary<string, byte>[] auntAnalysis)
+    {
+        for (uint i = 0; i < auntAnalysis.Length; i++)
+        {
+            bool isSameAsTargetAunt = auntAnalysis[i]
+                .All((KeyValuePair<string, byte> properties) =>
+                {
+                    return !targetAuntProperties.TryGetValue(properties.Key, out byte value)
+                        || value == properties.Value;
+                });
+
+            if (isSameAsTargetAunt)
+            {
+                return i + 1;
+            }
+        }
+
+        return null;
+    }
+}
