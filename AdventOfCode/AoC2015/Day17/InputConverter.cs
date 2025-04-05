daf6b79a24c357d72c2f5628a77a6c4edeced60e
Kassma <doerfer.cedric@gmail.com>
2025-04-05 15:10:40 +0200

    Day17: Implemented InputConverter

---
+++ AdventOfCode/AoC2015/Day17/InputConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode.AoC2015.Day17;
+
+internal static class InputConverter
+{
+    public static ushort[] Convert(string inputString)
+    {
+        return inputString
+            .Trim()
+            .Split('\n')
+            .Select((string line) =>
+            {
+                return ushort.TryParse(line.Trim(), out ushort value)
+                    ? value
+                    : throw new FormatException($"unable to convert line '{line}' to an ushort");
+            })
+            .ToArray();
+    }
+}
---
+++ AdventOfCodeTests/AoC2015/Day17/TestInputConverter.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCodeTests.AoC2015.Day17;
+
+using AdventOfCode.AoC2015.Day17;
+
+[TestFixture]
+public static class TestInputConverter
+{
+    public static IEnumerable<TestCaseData> TestCases
+    {
+        get
+        {
+            yield return new TestCaseData(
+                "20\r\n" +
+                "15\r\n" +
+                "10\r\n" +
+                "5\r\n" +
+                "5")
+                .Returns(new ushort[] { 20, 15, 10, 5, 5 })
+                .SetName("Test01");
+        }
+    }
+
+    [TestCaseSource(nameof(TestCases))]
+    public static ushort[] Convert_ValidCases_ShouldReturnCorrectValues(string inputString)
+    {
+        return InputConverter.Convert(inputString);
+    }
+}
