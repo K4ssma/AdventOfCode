43a870611953fec2e42b60dad7679ce7c503663b
Kassma <doerfer.cedric@gmail.com>
2025-04-06 13:04:50 +0200

    Day18: Implemented InputConverter

---
+++ AdventOfCode/AoC2015/Day18/InputConverter.cs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode.AoC2015.Day18;
+
+internal static class InputConverter
+{
+    public static bool[,] Convert(string inputString)
+    {
+        var lines = inputString
+            .Trim()
+            .Split('\n')
+            .Select((line) => line.Trim())
+            .ToArray();
+
+        if (lines.Length == 0
+            || lines[0].Length == 0)
+        {
+            throw new FormatException("unable to convert input string to grid");
+        }
+
+        var grid = new bool[lines[0].Length, lines.Length];
+
+        for (var y = 0; y < lines.Length; y++)
+        {
+            if (lines[y].Length != lines[0].Length)
+            {
+                throw new FormatException($"line {y} ({lines}) has a different length");
+            }
+
+            for (var x = 0; x < lines[0].Length; x++)
+            {
+                if (lines[y][x] == '#')
+                {
+                    grid[y, x] = true;
+                }
+                else if (lines[y][x] != '.')
+                {
+                    throw new ArgumentException($"invalid char '{lines[y][x]}' in line '{y}'");
+                }
+            }
+        }
+
+        return grid;
+    }
+}
---
+++ AdventOfCodeTests/AoC2015/Day18/TestInputConverter.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCodeTests.AoC2015.Day18;
+
+using AdventOfCode.AoC2015.Day18;
+
+[TestFixture]
+public static class TestInputConverter
+{
+    public static IEnumerable<TestCaseData> TestCases
+    {
+        get
+        {
+            yield return new TestCaseData(
+                ".#.#.#\r\n" +
+                "...##.\r\n" +
+                "#....#\r\n" +
+                "..#...\r\n" +
+                "#.#..#\r\n" +
+                "####..")
+                .Returns(new bool[,]
+                {
+                    { false, true, false, true, false, true },
+                    { false, false, false, true, true, false },
+                    { true, false, false, false, false, true },
+                    { false, false, true, false, false, false },
+                    { true, false, true, false, false, true },
+                    { true, true, true, true, false, false },
+                })
+                .SetName("Test01");
+        }
+    }
+
+    [TestCaseSource(nameof(TestCases))]
+    public static bool[,] Convert_ValidCases_ShouldReturnCorrectGrid(string inputString)
+    {
+        return InputConverter.Convert(inputString);
+    }
+}
