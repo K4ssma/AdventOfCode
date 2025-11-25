2cee79ae560f4655b20480d15dc80396f412e8bb
Kassma <doerfer.cedric@gmail.com>
2025-11-25 18:49:27 +0100

    added "AocDayMock"-class
    - dummy implementation for testing the console app with dummy data

---
+++ ConsoleApp/AocDayMock.cs
@@ -0,0 +1,28 @@
+﻿namespace Kassma.AdventOfCode.ConsoleApp;
+
+using Kassma.AdventOfCode.Abstractions;
+
+/// <summary>
+///     Mockup implementation of the <see cref="IAocDay"/>-interface for providing dummy data.
+/// </summary>
+internal sealed class AocDayMock : IAocDay
+{
+    /// <inheritdoc/>
+    public static byte Day => 0;
+
+    /// <inheritdoc/>
+    public static string SolvePart01(string input)
+    {
+        Thread.Sleep(1000);
+
+        return "Dummy result for part 01";
+    }
+
+    /// <inheritdoc/>
+    public static string SolvePart02(string input)
+    {
+        Thread.Sleep(1000);
+
+        return "Dummy result for part 02";
+    }
+}
