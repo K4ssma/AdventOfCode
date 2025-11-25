97b41943f8f9251e9611e2d0f6314c0464822596
Kassma <doerfer.cedric@gmail.com>
2025-11-25 22:24:00 +0100

    added mock for the "IAocYear"-interface
    - provides dummy data for console ui testing

---
+++ ConsoleApp/Mocks/AocYear.cs
@@ -0,0 +1,19 @@
+﻿namespace Kassma.AdventOfCode.ConsoleApp.Mocks;
+
+using Kassma.AdventOfCode.Abstractions;
+
+/// <summary>
+///     Mock class implementing the <see cref="IAocYear"/>-interface for providing dummy data.
+/// </summary>
+internal sealed class AocYear : IAocYear
+{
+    /// <summary>
+    ///     Gets 0.
+    /// </summary>
+    public static uint Year => 0;
+
+    /// <summary>
+    ///     Gets an array only containing the <see cref="AocDay"/> at index 0.
+    /// </summary>
+    public static IAocDay?[] Days => [new AocDay()];
+}
