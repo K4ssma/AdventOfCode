a5016c2afae1f7f2ee80f813d401fff45d24a0ee
Kassma <doerfer.cedric@gmail.com>
2025-11-26 18:12:28 +0100

    added config file and simple ui without interaction

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,51 @@
 ﻿namespace Kassma.AdventOfCode.ConsoleApp;
 
+using Kassma.AdventOfCode.Abstractions;
+using Kassma.AdventOfCode.ConsoleApp.Mocks;
+using Microsoft.Extensions.Configuration;
+
 /// <summary>
 ///     Entry point for the application.
 /// </summary>
 internal static class Program
 {
+    private static readonly IAocYear[] Years =
+    [
+        new AocYear(),
+    ];
+
     private static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        var config = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json", false, true)
+            .Build();
+
+        var uiConfig = config
+            .GetSection("UiConfig")
+            .Get<UiConfig>();
+
+        ArgumentNullException.ThrowIfNull(uiConfig);
+
+        Console.WriteLine(
+            $"Note: You can enter \"{uiConfig.ExitCode}\" at any given time" +
+            " in order to stop the application.\r\n");
+
+        Console.WriteLine("Please enter the year of Advent of Code you would like to solve.");
+        Console.WriteLine("The following years have an available solver:");
+
+        for (var i = 0; i < Years.Length; i++)
+        {
+            Console.Write(Years[i].Year);
+
+            if (i < Years.Length - 1)
+            {
+                Console.Write(", ");
+            }
+            else
+            {
+                Console.Write("\r\n");
+            }
+        }
     }
 }
---
+++ ConsoleApp/UiConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace Kassma.AdventOfCode.ConsoleApp;
+
+/// <summary>
+///     Record for storing app relevant configurations.
+/// </summary>
+internal sealed record UiConfig
+{
+    /// <summary>
+    ///     Gets or sets the char, which the user can enter in order to exit the application at any given point.
+    /// </summary>
+    public required char ExitCode { get; set; }
+}
