963794840acf5625ec9e5ccb9786f873dafd0c07
Kassma <doerfer.cedric@gmail.com>
2025-12-01 15:40:52 +0100

    Added "ProgressStatus"-dto-record
    - used for showing progress to the user

---
+++ Abstractions/ProgressStatus.cs
@@ -0,0 +1,22 @@
+﻿namespace Kassma.AdventOfCode.Abstractions;
+
+/// <summary>
+///     Dto for passing progress information.
+/// </summary>
+public sealed record ProgressStatus
+{
+    /// <summary>
+    ///     Gets a value indicating whether this instance is part of a bigger status categorie or not.
+    /// </summary>
+    public required bool IsSubstatus { get; init; }
+
+    /// <summary>
+    ///     Gets the message which describes the current status.
+    /// </summary>
+    public required string StatusMessage { get; init; }
+
+    /// <summary>
+    ///     Gets the currents status categories completion status.
+    /// </summary>
+    public required byte StatusPercent { get; init; }
+}
