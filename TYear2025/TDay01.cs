92c3729b001050b533f270786ee958e65cdbf589
Kassma <doerfer.cedric@gmail.com>
2025-12-02 18:41:44 +0100

    added unit test for day01 part01

---
+++ TYear2025/TDay01.cs
@@ -0,0 +1,40 @@
+﻿namespace Kassma.AdventOfCode.TYears.TYear2025;
+
+using System.Text.RegularExpressions;
+using Kassma.AdventOfCode.Abstractions;
+using Kassma.AdventOfCode.Years.Year2025;
+using Moq;
+
+/// <summary>
+///     Unit Tests for the <see cref="Day01"/> class.
+/// </summary>
+[TestFixture]
+internal sealed class TDay01
+{
+    private const string TestInput =
+        "L68\r\n" +
+        "L30\r\n" +
+        "R48\r\n" +
+        "L5\r\n" +
+        "R60\r\n" +
+        "L55\r\n" +
+        "L1\r\n" +
+        "L99\r\n" +
+        "R14\r\n" +
+        "L82";
+
+    /// <summary>
+    ///     Unit Test for the <see cref="Day01.SolvePart01(IProgress{ProgressStatus}, string)"/> method.
+    /// </summary>
+    [Test]
+    public void TSolvePart01()
+    {
+        var progressMock = new Mock<IProgress<ProgressStatus>>();
+        var day01Solver = new Day01();
+
+        var result = day01Solver.SolvePart01(progressMock.Object, TestInput);
+        var parsedResult = int.Parse(Regex.Match(result, @"\d").Value);
+
+        Assert.That(parsedResult, Is.EqualTo(3));
+    }
+}
