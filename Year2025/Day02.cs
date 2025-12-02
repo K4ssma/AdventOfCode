651efdd80ace700ff576cbe82779cba180285a28
Kassma <doerfer.cedric@gmail.com>
2025-12-02 21:09:49 +0100

    added solver for day02 part02

--- Year2025/Day02.cs
+++ Year2025/Day02.cs
@@ -53,9 +53,9 @@ public sealed class Day02 : IAocDay
                 var digits = (int)Math.Log10(id) + 1;
 
                 if (digits % 2 != 0)
-                    {
-                        continue;
-                    }
+                {
+                    continue;
+                }
 
                 var firstGroup = id / (ulong)Math.Pow(10, digits / 2);
                 var leftSideMask = firstGroup * (ulong)Math.Pow(10, digits / 2);
@@ -63,8 +63,8 @@ public sealed class Day02 : IAocDay
                 var secondGroup = id - leftSideMask;
 
                 if (firstGroup == secondGroup)
-                    {
-                        sum += id;
+                {
+                    sum += id;
                 }
             }
         }
@@ -82,6 +82,95 @@ public sealed class Day02 : IAocDay
     /// <inheritdoc/>
     public string SolvePart02(IProgress<ProgressStatus> progress, string input)
     {
-        throw new NotImplementedException();
+        progress.Report(new()
+        {
+            IsHeadStatus = true,
+            StatusMessage = "Parsing input",
+            StatusPercent = 0,
+        });
+
+        var ranges = input
+            .Split(',')
+            .Select((rangeString) =>
+            {
+                var rangeParts = rangeString.Split('-');
+
+                return (ulong.Parse(rangeParts[0]), ulong.Parse(rangeParts[1]));
+            })
+            .ToArray();
+
+        progress.Report(new()
+        {
+            IsHeadStatus = true,
+            StatusMessage = $"Solving process (checking {ranges.Length} ranges)",
+            StatusPercent = 0,
+        });
+
+        var sum = 0ul;
+
+        for (var i = 0; i < ranges.Length; i++)
+        {
+            (var min, var max) = ranges[i];
+
+            progress.Report(new()
+            {
+                IsHeadStatus = false,
+                StatusMessage = $"Checking range {min}-{max}",
+                StatusPercent = (byte)(i * (100 / ranges.Length)),
+            });
+
+            for (var id = min; id <= max; id++)
+            {
+                var digits = (int)Math.Log10(id) + 1;
+
+                if (digits % 2 != 0)
+                {
+                    continue;
+                }
+
+                for (var splitGroups = 2; splitGroups <= digits; splitGroups++)
+                {
+                    if (digits % splitGroups != 0)
+                    {
+                        continue;
+                    }
+
+                    var digitsPerGroup = digits / splitGroups;
+                    var compareNumber = id / (ulong)Math.Pow(10, digitsPerGroup * (splitGroups - 1));
+                    var isPattern = true;
+
+                    for (var group = 0; group < splitGroups - 1; group++)
+                    {
+                        var leftSideMask = id / (ulong)Math.Pow(10, digitsPerGroup * (group + 1));
+                        leftSideMask *= (ulong)Math.Pow(10, digitsPerGroup * (group + 1));
+
+                        var rightSideMask = group == 0
+                            ? 0
+                            : id / (ulong)Math.Pow(10, digitsPerGroup * (group - 1));
+
+                        if (id - leftSideMask - rightSideMask != compareNumber)
+                        {
+                            isPattern = false;
+                            break;
+                        }
+                    }
+
+                    if (isPattern)
+                    {
+                        sum += id;
+                        break;
+                    }
+                }
+            }
+        }
+
+        progress.Report(new()
+        {
+            IsHeadStatus = true,
+            StatusMessage = "Done",
+            StatusPercent = 100,
+        });
+
+        return sum.ToString();
     }
 }
