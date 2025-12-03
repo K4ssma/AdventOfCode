ff37eaecd66a30f2e0ce998457a21f373c852c65
Kassma <doerfer.cedric@gmail.com>
2025-12-03 14:25:08 +0100

    added unit test for day03 part01

---
+++ TYear2025/TDay03.cs
@@ -0,0 +1,32 @@
+﻿namespace Kassma.AdventOfCode.TYears.TYear2025;
+
+using Kassma.AdventOfCode.Abstractions;
+using Kassma.AdventOfCode.Years.Year2025;
+using Moq;
+
+/// <summary>
+///     Unit Tests for <see cref="Day03"/> class.
+/// </summary>
+[TestFixture]
+internal sealed class TDay03
+{
+    private const string TestInput =
+        "987654321111111\r\n" +
+        "811111111111119\r\n" +
+        "234234234234278\r\n" +
+        "818181911112111";
+
+    /// <summary>
+    ///     Unit Test for <see cref="Day03.SolvePart01(IProgress{ProgressStatus}, string)"/> method.
+    /// </summary>
+    [Test]
+    public void TSolvePart01()
+    {
+        var progressMock = new Mock<Progress<ProgressStatus>>();
+        var aocDay03 = new Day03();
+
+        var result = int.Parse(aocDay03.SolvePart01(progressMock.Object, TestInput));
+
+        Assert.That(result, Is.EqualTo(357));
+    }
+}
