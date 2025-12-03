11de520c2a25e0e02de6e8b3a9427e2822df0b2f
Kassma <doerfer.cedric@gmail.com>
2025-12-03 15:11:28 +0100

    refactored day03 to solve both partsof the challenge

--- Year2025/Day03.cs
+++ Year2025/Day03.cs
@@ -1,7 +1,6 @@
 ﻿namespace Kassma.AdventOfCode.Years.Year2025;
 
 using System;
-using System.Diagnostics;
 using Kassma.AdventOfCode.Abstractions;
 
 /// <summary>
@@ -11,6 +10,17 @@ public sealed class Day03 : IAocDay
 {
     /// <inheritdoc/>
     public string SolvePart01(IProgress<ProgressStatus> progress, string input)
+    {
+        return GetMaxJoltage(progress, input, 2).ToString();
+    }
+
+    /// <inheritdoc/>
+    public string SolvePart02(IProgress<ProgressStatus> progress, string input)
+    {
+        return GetMaxJoltage(progress, input, 12).ToString();
+    }
+
+    private static ulong GetMaxJoltage(IProgress<ProgressStatus> progress, string input, int batteriesPerBank)
     {
         progress.Report(new()
         {
@@ -28,19 +38,48 @@ public sealed class Day03 : IAocDay
             StatusPercent = 0,
         });
 
-        var sum = 0;
+        var sum = 0ul;
 
         for (var bankIndex = 0; bankIndex < banks.Length; bankIndex++)
         {
             var bank = banks[bankIndex];
 
-            var firstDigitIndex = GetFirstHighestDigitIndex(
-                bank, 0, bank.Length - 1, out var firstDigit);
+            if (bank.Length < batteriesPerBank)
+            {
+                throw new ArgumentException(
+                    $"Bank \"{bank}\" does not have enough batteries to activate {batteriesPerBank} batteries");
+            }
 
-            GetFirstHighestDigitIndex(
-                bank, firstDigitIndex + 1, bank.Length, out var secondDigit);
+            var previousBatteryIndex = 0;
 
-            sum += (firstDigit * 10) + secondDigit;
+            for (var battery = 0; battery < batteriesPerBank; battery++)
+            {
+                var highestDigit = default(int?);
+                var highestDigitIndex = default(int?);
+                var currIndex = battery == 0
+                    ? 0
+                    : previousBatteryIndex + 1;
+
+                do
+                {
+                    var parsedDigit = int.Parse(bank[currIndex].ToString());
+
+                    if (highestDigit is null
+                        || parsedDigit > highestDigit)
+                    {
+                        highestDigit = parsedDigit;
+                        highestDigitIndex = currIndex;
+                    }
+
+                    currIndex++;
+                }
+                while (highestDigit != 9
+                    && currIndex <= bank.Length - (batteriesPerBank - battery));
+
+                sum += (ulong)(highestDigit * Math.Pow(10, batteriesPerBank - battery - 1));
+
+                previousBatteryIndex = highestDigitIndex!.Value;
+            }
 
             progress.Report(new()
             {
@@ -57,37 +96,6 @@ public sealed class Day03 : IAocDay
             StatusPercent = 100,
         });
 
-        return sum.ToString();
-    }
-
-    /// <inheritdoc/>
-    public string SolvePart02(IProgress<ProgressStatus> progress, string input)
-    {
-        throw new NotImplementedException();
-    }
-
-    private static int GetFirstHighestDigitIndex(string numberString, int startIndex, int endIndex, out int highestDigit)
-    {
-        highestDigit = 0;
-        var highestDigitIndex = default(int?);
-        var currIndex = startIndex;
-
-        do
-        {
-            var parsedDigit = int.Parse(numberString[currIndex].ToString());
-
-            if (highestDigitIndex is null
-                || parsedDigit > highestDigit)
-            {
-                highestDigit = parsedDigit;
-                highestDigitIndex = currIndex;
-            }
-
-            currIndex++;
-        }
-        while (currIndex < endIndex
-            && highestDigit != 9);
-
-        return highestDigitIndex.Value;
+        return sum;
     }
 }
