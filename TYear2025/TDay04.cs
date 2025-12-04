003dee5e45e8e1f104b37bddec8bfcb9dc06e658
Kassma <doerfer.cedric@gmail.com>
2025-12-04 15:32:42 +0100

    day04: added unit test for part01

---
+++ TYear2025/TDay04.cs
@@ -0,0 +1,38 @@
+﻿namespace Kassma.AdventOfCode.TYears.TYear2025;
+
+using Kassma.AdventOfCode.Abstractions;
+using Kassma.AdventOfCode.Years.Year2025;
+using Moq;
+
+/// <summary>
+///     Unit Tests for <see cref="Day04"/> class.
+/// </summary>
+[TestFixture]
+internal sealed class TDay04
+{
+    private const string TestInput =
+        "..@@.@@@@.\r\n" +
+        "@@@.@.@.@@\r\n" +
+        "@@@@@.@.@@\r\n" +
+        "@.@@@@..@.\r\n" +
+        "@@.@@@@.@@\r\n" +
+        ".@@@@@@@.@\r\n" +
+        ".@.@.@.@@@\r\n" +
+        "@.@@@.@@@@\r\n" +
+        ".@@@@@@@@.\r\n" +
+        "@.@.@@@.@.";
+
+    /// <summary>
+    ///     Unit Test for <see cref="Day04.SolvePart01(IProgress{ProgressStatus}, string)"/> method.
+    /// </summary>
+    [Test]
+    public void TSolvePart01()
+    {
+        var progressMock = new Mock<Progress<ProgressStatus>>();
+        var aocDay04 = new Day04();
+
+        var result = int.Parse(aocDay04.SolvePart01(progressMock.Object, TestInput));
+
+        Assert.That(result, Is.EqualTo(13));
+    }
+}
